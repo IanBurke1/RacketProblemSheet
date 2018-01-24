63a9cd6f252ba092dd2f524eff917328effaa242
Ian Burke <iano.burke@gmail.com>
2018-01-24 13:37:01 +0000

    Q1 completed

--- Exercises.rkt
+++ Exercises.rkt
@@ -1,9 +1,23 @@
 #lang racket
 
+; Q.1
 ;(i) (3×(5+(10÷5)))
-(/ 10 5)
+(+ 5(* 3(/ 10 5)))
+
 ;(ii) (2+3+4+5)
+(+ 2 3 4 5)
+
 ;(iii) (1+(5+(2+(10÷3))))
+(+ 2 5 1(/ 10 3))
+
 ;(iv) (1+(5+(2+(10÷3.0))))
+(+ 2 5 1(/ 10 3.0))
+
 ;(v) (3+5)×(10÷2)
+(*(+ 3 5)(/ 10 2))
+
 ;(vi) (3+5)×(10÷2)+(1+(5+(2+(10÷3))))
+(+(*(+ 3 5)(/ 10 2))(+ 2 5 1(/ 10 3)))
+
+; Q.2 Define a procedure discount that takes two arguments: an item’s initial price and a percentage discount
+(define discount 
