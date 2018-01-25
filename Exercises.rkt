15fb674bb356d589edf43b82a109f5bb4a2882c1
Ian Burke <iano.burke@gmail.com>
2018-01-25 16:10:39 +0000

    Q2 completed

--- Exercises.rkt
+++ Exercises.rkt
@@ -20,4 +20,8 @@
 (+(*(+ 3 5)(/ 10 2))(+ 2 5 1(/ 10 3)))
 
 ; Q.2 Define a procedure discount that takes two arguments: an item’s initial price and a percentage discount
-(define discount 
+(define (discount x dis)(- x(* x(/ dis 100))))
+(discount 10 5)
+(discount  29.50 50)
+
+; Q.3 Define a function grcomdiv that takes two integers and returns their greatest common divisor.
